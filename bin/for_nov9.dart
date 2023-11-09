69161893c362b756144c1bf71b14c7c5921de634
BeeJet1 <bekzatbaiysh@gmail.com>
2023-11-09 23:54:07 +0600

    first commit

---
+++ bin/for_nov9.dart
@@ -0,0 +1,110 @@
+import 'package:for_nov9/for_nov9.dart' as for_nov9;
+
+import 'dart:io';
+
+import 'package:for_2/for_2.dart' as for_2;
+
+void main(List<String> arguments) {
+  
+  /*for (int i = 10; i <= 20; i++) {
+    print(i * i); 
+  }*/
+  /*int n = int.parse(stdin.readLineSync()!);
+  int summ = 0;
+  for (int i = 1; i <= n; i++) {
+    summ += i;
+  }
+  print(summ);*/ 
+
+  /*print('enter');
+  double s = double.parse(stdin.readLineSync()!);
+  print('enter frequency');
+  int nn = int.parse(stdin.readLineSync()!);
+  print('enter frequency');
+  for (int i = 1; i <= nn; i++) {
+    s = s + (s * 0.03);
+    print('$i month $s');*/ 
+
+  /*for (int i = 20; i <= 50; i++) {
+    if (i % 3 == 0 && i % 5 != 0) {
+      print(i); //! is nor equal to 0?
+    } //% is using for deving number with full number
+  }*/ 
+  /*int summ = 0;
+  for (int i = 1; i <= 50; i++) {
+    if (i % 5 == 0 || i % 7 == 0) {
+      summ++;
+    }
+  }
+  print(summ);*/
+
+  /*for (int i = 10; i <= 99; i++) {
+    if(i % 4 == 0 && i % 6 != 0) {
+      print(i);
+    }
+  }*/
+
+  /*int summ = 0;
+  for(int i = 100; i <= 200; i++) {
+    if (i % 17 ==0) {
+      summ +=i;
+    }
+  }
+  print(summ);*/
+
+  /*print('enter number');
+  int n = int.parse(stdin.readLineSync()!);
+  int summ = 0;
+  for (int i = 1; i <= n; i++) {
+    summ = summ + (i * i);
+  }
+  print(summ);*/
+
+  /*print('first number');
+  int n = int.parse(stdin.readLineSync()!);
+  print('second number');
+  int n2 = int.parse(stdin.readLineSync()!);
+  int summ = 0;
+  for (int i = n; i <= n2; i++) {
+    summ += i;
+  }
+  print(summ);*/
+
+  /* print('enter a year');
+  int year = int.parse(stdin.readLineSync()!);
+  if (year % 4 ==0) {
+    print('danger');
+  } else {
+    print('non');
+  }*/
+
+  /*print('enter a number 1');
+  int a = int.parse(stdin.readLineSync()!);
+  print('enter a number 2');
+  int b = int.parse(stdin.readLineSync()!);
+  
+  for (int i = a; i > -1; i++) {
+    if (i % a == 0 && i % b == 0) {
+      print(i);
+      break;
+    }
+  }*
+
+  /*int a = 123;
+  String b = a.toString();
+  int summ = 0;
+
+  for (int i = 0; i < b.length; i++) {
+    summ += int.parse(b[i]);
+  }
+  print(summ);*/
+
+  /*String a = 'weww';
+
+  String b = a.split('').reversed.toList().join();
+  if (a == b) {
+    print('palendrom');
+  } else {
+    print('non');
+  }  */ 
+}
---
+++ test/for_nov9_test.dart
@@ -0,0 +1,8 @@
+import 'package:for_nov9/for_nov9.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
